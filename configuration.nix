24576b7e474e8caa9644bc271ad7cfc4251853df
Gordon Campbell <gordy2007@hotmail.co.uk>
2026-06-16 22:00:24 +0100

    zram

--- configuration.nix
+++ configuration.nix
@@ -136,6 +136,10 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
+zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+    memoryPercent = 30;
+  };
 
 }
