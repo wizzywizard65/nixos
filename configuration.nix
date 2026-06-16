24b6831f7c7cbcfc8999c9963c7070f499d28551
Gordon Campbell <gordy2007@hotmail.co.uk>
2026-06-16 22:02:20 +0100

    remove zram

--- configuration.nix
+++ configuration.nix
@@ -136,10 +136,6 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-zramSwap = {
-    enable = true;
-    algorithm = "zstd";
-    memoryPercent = 30;
-  };
+
 
 }
