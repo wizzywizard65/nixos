75789faec1532d25172733d84031619a46066525
Gordy Campbell <gordy2007@hotmail.co.uk>
2026-09-10 19:36:28 +0100

    update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -8,30 +8,30 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sr_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "xhci_pci" "usbhid" "usb_storage" "sd_mod" "sr_mod" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/1af6bb11-f9eb-4d36-b401-b512e64bf987";
+    { device = "/dev/disk/by-uuid/71776215-5c56-4b1b-ac23-f41a8adf28b3";
       fsType = "btrfs";
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/1af6bb11-f9eb-4d36-b401-b512e64bf987";
+    { device = "/dev/disk/by-uuid/71776215-5c56-4b1b-ac23-f41a8adf28b3";
       fsType = "btrfs";
       options = [ "subvol=home" ];
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/1af6bb11-f9eb-4d36-b401-b512e64bf987";
+    { device = "/dev/disk/by-uuid/71776215-5c56-4b1b-ac23-f41a8adf28b3";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/304fdf60-89e7-4eed-b1c2-9353c53ba13b"; }
+    [ { device = "/dev/disk/by-uuid/a50948a9-3d32-40e4-9fcd-b5e4174fc529"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
